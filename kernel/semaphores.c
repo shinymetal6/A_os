01dd0428922d4e3a418a7679cbb0f03de0a058c4
Fil <f.visocchi@stonehex.com>
2023-09-23 09:00:08 +0200

    semaphores fixed

--- kernel/semaphores.c
+++ kernel/semaphores.c
@@ -33,7 +33,6 @@ uint32_t destroy_semaphore(uint8_t semaphore_id)
 	Semaphores.semaphore[semaphore_id] = 0;
 	if ( Semaphores.semaphore_waiting_process[semaphore_id] )
 		activate_process(Semaphores.semaphore_waiting_process[semaphore_id],WAKEUP_FROM_SEMAPHORE,0);
-
 	return 0;
 }
 
@@ -50,6 +49,7 @@ uint32_t get_semaphore(uint8_t semaphore_id,uint8_t semaphore_flag,uint32_t sema
 uint32_t	ret_val = SEMAPHORE_RED;
 uint32_t	wakeup;
 
+	__disable_irq();
 	if ( (semaphore_flag & SEMAPHORE_SUSPEND_IF_RED) != SEMAPHORE_SUSPEND_IF_RED)
 	{
 		if (Semaphores.semaphore[semaphore_id] == SEMAPHORE_FREE )
@@ -74,19 +74,26 @@ uint32_t	wakeup;
 				{
 					Semaphores.semaphore_timeout[semaphore_id] = Asys.g_tick_count + semaphore_timeout;
 					Semaphores.semaphore_waiting_process[semaphore_id] = Asys.current_process;
-					wakeup = wait_event( EVENT_SEMAPHORE | EVENT_SEMAPHORE_TIMEOUT );
-					if (( wakeup & WAKEUP_FROM_SEMAPHORE_TIMEOUT) == WAKEUP_FROM_SEMAPHORE_TIMEOUT )
+					wakeup = 0;
+					while(1)
 					{
-						Semaphores.semaphore_waiting_process[semaphore_id] = 0;
-						ret_val = SEMAPHORE_TIMEOUT;
+						while ( (wakeup & (WAKEUP_FROM_SEMAPHORE | WAKEUP_FROM_SEMAPHORE_TIMEOUT)) == 0)
+							wakeup = wait_event( EVENT_SEMAPHORE | EVENT_SEMAPHORE_TIMEOUT );
+						if (( wakeup & WAKEUP_FROM_SEMAPHORE_TIMEOUT) == WAKEUP_FROM_SEMAPHORE_TIMEOUT )
+						{
+							Semaphores.semaphore_waiting_process[semaphore_id] = 0;
+							ret_val = SEMAPHORE_TIMEOUT;
+							break;
+						}
+						else if (( wakeup & WAKEUP_FROM_SEMAPHORE) == WAKEUP_FROM_SEMAPHORE )
+						{
+							Semaphores.semaphore_waiting_process[semaphore_id] = 0;
+							ret_val = SEMAPHORE_GREEN;
+							break;
+						}
+						else
+							sem_err++;
 					}
-					else if (( wakeup & WAKEUP_FROM_SEMAPHORE) == WAKEUP_FROM_SEMAPHORE )
-					{
-						Semaphores.semaphore_waiting_process[semaphore_id] = 0;
-						ret_val = SEMAPHORE_GREEN;
-					}
-					else
-						sem_err++;
 				}
 				else
 				{
@@ -108,6 +115,7 @@ uint32_t	wakeup;
 				ret_val = SEMAPHORE_UNAVAILABLE;
 		}
 	}
+	__enable_irq();
 	return ret_val;
 }
 
@@ -115,6 +123,7 @@ void check_semaphores(void)
 {
 register uint8_t	i,j;
 
+	__disable_irq();
 	for( i = 1 ; i < MAX_PROCESS ; i++)
 	{
 		if((process[i].wait_event & SUSPEND_ON_SEMAPHORE) == SUSPEND_ON_SEMAPHORE)
@@ -129,5 +138,6 @@ register uint8_t	i,j;
 			}
 		}
 	}
+	__enable_irq();
 }
 
