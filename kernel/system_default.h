ad456456fcbe6fbbf520355fdd6b3e4a8808c580
Fil <shinymetal6@gmail.com>
2024-07-10 15:48:20 +0200

    updated A_OS_VERSION to v2024.07.00-rc2

--- kernel/system_default.h
+++ kernel/system_default.h
@@ -136,6 +136,6 @@
 #define	Exti1_PRIORITY			13
 #define	Exti2_PRIORITY			13
 // versioning
-#define	A_OS_VERSION			"v2024.07.00-rc1"
+#define	A_OS_VERSION			"v2024.07.00-rc2"
 
 #endif /* KERNEL_SYSTEM_DEFAULT_H_ */
