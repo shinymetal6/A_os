4d176288ce2b1970d9600696b708a2723a5babea
Fil <shinymetal6@gmail.com>
2024-09-02 17:47:19 +0200

    On v2024.09.00.rc1

--- kernel/system_default.h
+++ kernel/system_default.h
@@ -142,6 +142,6 @@
 #define	Exti1_PRIORITY			13
 #define	Exti2_PRIORITY			13
 // versioning
-#define	A_OS_VERSION			"v2024.09.00-rc0"
+#define	A_OS_VERSION			"v2024.09.00-rc1"
 
 #endif /* KERNEL_SYSTEM_DEFAULT_H_ */
