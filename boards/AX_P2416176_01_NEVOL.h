0952a0b0e8b779a8960dc123bbeb6b712a8b9496
Fil <shinymetal6@gmail.com>
2025-03-14 14:00:13 +0100

    Fixed nevol board

--- boards/AX_P2416176_01_NEVOL.h
+++ boards/AX_P2416176_01_NEVOL.h
@@ -38,6 +38,8 @@
 	//#define	USB_MIDI			1
 #endif // #ifdef USB_ENABLED
 
+#define AUDIO_ENABLED	1
+
 #define DTCM_VECTORS_DATA	__attribute__((section(".dtcm_data")))   __attribute__ ((aligned (32)))
 #define DWNLD_DATA_AREA		__attribute__((section(".d2ram")))   	 __attribute__ ((aligned (32)))
 #define ETH_DATA_AREA		__attribute__((section(".d2ram")))   	 __attribute__ ((aligned (32)))
