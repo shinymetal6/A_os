bfe92a8063c6e1305f0ee1ab27f6bb6bc732046f
Fil <shinymetal6@gmail.com>
2025-06-25 09:03:04 +0200

    On v2025.06

--- kernel/system_default.h
+++ kernel/system_default.h
@@ -91,6 +91,6 @@
 #define	Exti1_PRIORITY			13
 #define	Exti2_PRIORITY			13
 // versioning
-#define	A_OS_VERSION			"v2025.06.00-rc2"
+#define	A_OS_VERSION			"v2025.06"
 
 #endif /* KERNEL_SYSTEM_DEFAULT_H_ */
