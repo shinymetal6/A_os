f5602425909b4a93d01efe98d04616b4931afa09
Fil <shinymetal6@gmail.com>
2025-08-02 10:06:45 +0200

    updated README and version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # A_os
-v2025.09.00.rc1<br>
+v2025.09.00.rc2<br>
 !!! NOTE !!!<br>
 Still this runs with <br>
 STM32CubeIDE Version: 1.18.1 Build: 24813_20250409_2138 (UTC)<br>
--- kernel/system_default.h
+++ kernel/system_default.h
@@ -91,6 +91,6 @@
 #define	Exti1_PRIORITY			13
 #define	Exti2_PRIORITY			13
 // versioning
-#define	A_OS_VERSION			"v2025.09.00-rc1"
+#define	A_OS_VERSION			"v2025.09.00-rc2"
 
 #endif /* KERNEL_SYSTEM_DEFAULT_H_ */
